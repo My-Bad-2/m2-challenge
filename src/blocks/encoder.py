70d11bcc0281a2c66cdb7371ad126bd1ca8db5f4
Yash Verma <yashverma056@proton.me>
2025-11-07 12:15:45 -0500

    Refactor encoder.py

--- src/blocks/encoder.py
+++ src/blocks/encoder.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-import flax.linen as nnx
+from flax import nnx
 from .gqa import SparseMoE, TimeAwareGQA, GQA
 
 
@@ -8,36 +8,31 @@ class TimeAwareEncoderBlock(nnx.Module):
     Transformer Encoder block that uses TimeAwareGQA
     """
 
-    config: dict
+    def __init__(self, config: dict, *, rngs: nnx.Rngs):
+        self.norm1 = nnx.LayerNorm(
+            num_features=config["embed_dim"], epsilon=1e-6, rngs=rngs
+        )
+        self.attn = TimeAwareGQA(
+            embed_dim=config["embed_dim"],
+            num_heads=config["num_heads"],
+            num_kv_groups=config["num_kv_groups"],
+            rngs=rngs,
+        )
 
-    @nnx.compact
+        self.norm2 = nnx.LayerNorm(
+            num_features=config["embed_dim"], epsilon=1e-6, rngs=rngs
+        )
+        self.moe = SparseMoE(
+            num_experts=config["num_experts"],
+            experts_per_token=config["experts_per_token"],
+            embed_dim=config["embed_dim"],
+            hidden_dim=config["embed_dim"] * 4,
+            rngs=rngs,
+        )
+
     def __call__(self, x, session_deltas, mask=None):
-        embed_dim = self.config["embed_dim"]
-        num_heads = self.config["num_heads"]
-        num_kv_groups = self.config["num_kv_groups"]
-        num_experts = self.config["num_experts"]
-        experts_per_token = self.config["experts_per_token"]
-        ffn_hidden_dim = embed_dim * 4
-
-        norm1 = nnx.LayerNorm(epsilon=1e-6)
-        attn = TimeAwareGQA(
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            num_kv_groups=num_kv_groups,
-            name="time_aware_gqa",
-        )
-
-        norm2 = nnx.LayerNorm(epsilon=1e-6)
-        moe = SparseMoE(
-            num_experts=num_experts,
-            experts_per_token=experts_per_token,
-            embed_dim=embed_dim,
-            hidden_dim=ffn_hidden_dim,
-            name="sparse_moe_ffn",
-        )
-
-        x_norm = norm1(x)
-        attn_output = attn(
+        x_norm = self.norm1(x)
+        attn_output = self.attn(
             query=x_norm,
             value=x_norm,
             key=x_norm,
@@ -46,8 +41,8 @@ class TimeAwareEncoderBlock(nnx.Module):
         )
 
         x = x + attn_output
-        x_norm = norm2(x)
-        ffn_output = moe(x_norm)
+        x_norm = self.norm2(x)
+        ffn_output = self.moe(x_norm)
         x = x + ffn_output
 
         return x
@@ -58,60 +53,58 @@ class StandardDecoderBlock(nnx.Module):
     Standard Pre-LN Transformer Decoder block
     """
 
+    def __init__(self, config: dict, *, rngs: nnx.Rngs):
+        self.norm1 = nnx.LayerNorm(
+            num_features=config["embed_dim"], epsilon=1e-6, rngs=rngs
+        )
+        self.self_attn = GQA(
+            embed_dim=config["embed_dim"],
+            num_heads=config["num_heads"],
+            num_kv_groups=config["num_kv_groups"],
+            use_casual_mask=True,
+            rngs=rngs,
+        )
+
+        self.norm2 = nnx.LayerNorm(config["embed_dim"], epsilon=1e-6, rngs=rngs)
+
+        self.cross_attn = GQA(
+            embed_dim=config["embed_dim"],
+            num_heads=config["num_heads"],
+            num_kv_groups=config["num_kv_groups"],
+            use_casual_mask=False,
+            rngs=rngs,
+        )
+
+        self.norm3 = nnx.LayerNorm(
+            num_features=config["embed_dim"], epsilon=1e-6, rngs=rngs
+        )
+        self.moe = SparseMoE(
+            num_experts=config["num_experts"],
+            experts_per_token=config["experts_per_token"],
+            embed_dim=config["embed_dim"],
+            hidden_dim=config["embed_dim"] * 4,
+            rngs=rngs,
+        )
+
     config: dict
 
-    @nnx.compact
     def __call__(self, x, encoder_context, x_mask=None, context_mask=None):
-        embed_dim = self.config["embed_dim"]
-        num_heads = self.config["num_heads"]
-        num_kv_groups = self.config["num_kv_groups"]
-        num_experts = self.config["num_experts"]
-        experts_per_token = self.config["experts_per_token"]
-        ffn_hidden_dim = embed_dim * 4
-
-        norm1 = nnx.LayerNorm(epsilon=1e-6)
-        self_attn = GQA(
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            num_kv_groups=num_kv_groups,
-            use_casual_mask=True,
-            name="self_attn_gqa",
-        )
-
-        norm2 = nnx.LayerNorm(epsilon=1e-6)
-        cross_attn = GQA(
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            num_kv_groups=num_kv_groups,
-            use_casual_mask=False,
-            name="cross_attn_gqa",
-        )
-
-        norm3 = nnx.LayerNorm(epsilon=1e-6)
-        moe = SparseMoE(
-            num_experts=num_experts,
-            experts_per_token=experts_per_token,
-            embed_dim=embed_dim,
-            hidden_dim=ffn_hidden_dim,
-            name="sparse_moe_ffn",
-        )
-
-        x_norm = norm1(x)
-        self_attn_output = self_attn(
+        x_norm = self.norm1(x)
+        self_attn_output = self.self_attn(
             query=x_norm, value=x_norm, key=x_norm, mask=x_mask
         )
 
         x = x + self_attn_output
 
-        x_norm = norm2(x)
-        cross_attn_output = cross_attn(
+        x_norm = self.norm2(x)
+        cross_attn_output = self.cross_attn(
             query=x_norm, value=encoder_context, key=encoder_context, mask=context_mask
         )
 
         x = x + cross_attn_output
 
-        x_norm = norm3(x)
-        ffn_output = moe(x_norm)
+        x_norm = self.norm3(x)
+        ffn_output = self.moe(x_norm)
 
         x = x + ffn_output
         return x
